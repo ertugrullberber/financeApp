4f5bcb69dacac5c9bef726d4a5e088391abbb025
ertugrullberber <ertugrulberber3461@gmail.com>
2022-05-02 19:18:29 +0300

    Initial Commit

---
+++ financeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  financeApp
+//
+//  Created by Ertugrul Berber on 2.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ financeApp/financeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  financeAppApp.swift
+//  financeApp
+//
+//  Created by Ertugrul Berber on 2.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct financeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
